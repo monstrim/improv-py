85aad0f7b5c9df91fef16279e74ecbba738ed9c2
Pedro Monstrinho Araújo <monstrim@gmail.com>
2025-10-14 13:32:22 -0300

    begin Improv class

---
+++ improv/improv.py
@@ -0,0 +1,12 @@
+class Improv:
+    def __init__ (
+            self, 
+            snippets:dict, 
+            ):
+        self.snippets:dict = dict(snippets)
+        
+    
+    def gen (self, snippetName:str, model=None) -> str:
+        output = ...
+        
+        return output
