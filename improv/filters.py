27ef0d70b2cd5e5ff14b18033e1cc1a621ceefb8
Pedro Monstrinho Araújo <monstrim@gmail.com>
2025-10-15 17:37:46 -0300

    generic comparer

--- improv/filters.py
+++ improv/filters.py
@@ -16,16 +16,25 @@ def __compareTags (a:list, b:list) -> TagComparison:
     return TagComparison.PARTIAL
 
 
+def __groupComparer(comparisonMode, bonus:int, cumulative:bool=False):
+    '''
+    bonus=None eliminates groups, e.g. for mismatched filter. bonus can be negative for penalizing.
+    '''
+    def _fn(group, model, improv):
+        score = 0
+        for groupTag in group['tags']:
+            for modelTag in model.tags:
+                if __compareTags(groupTag, modelTag) == comparisonMode:
+                    if not cumulative or bonus is None:
+                        return bonus
+                    score += bonus
+        return score
+    return _fn
+
+
 def mismatchFilter ():
     '''
     Looks for mismatched tags (i.e., tags which match the first position, and are therefore equivalent,
     but with a different sub-tag).
     '''
-    def _fn(group, model, improv):
-        for groupTag in group['tags']:
-            for modelTag in model.tags:
-                if __compareTags(groupTag, modelTag) == TagComparison.MISMATCH:
-                    return None
-        return 0
-
-    return _fn
+    return __groupComparer(comparisonMode=TagComparison.MISMATCH, bonus=None, cumulative=False)
