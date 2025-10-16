dc944534beed67c90a8a6b718ef0f8f402c6683b
Pedro Monstrinho Araújo <monstrim@gmail.com>
2025-10-15 21:32:13 -0300

    0.2

--- improv/__init__.py
+++ improv/__init__.py
@@ -1,8 +1,16 @@
 '''
-A Python port of [Bruno Dias\'s Improv] library for JavaScript.
+A Python port of Bruno Dias's Improv library for JavaScript. 
+Improv is a model-backed generative text grammar tool: Improv is similar to 
+Tracery in that it can generate random, procedurally generated text 
+recursively. Also like Tracery, Improv includes some basic templating 
+functionality.
+
+Unlike Tracery, however, Improv generators refer to *models* to build text. 
+This allows for more sophisticated text generation by referencing an underlying 
+world model.
 '''
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Pedro Monstrinho Araujo'
 
 
